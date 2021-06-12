70609724c35a42c712f0cd643817eb6612484b8f
bhavan1200/Project2 <bhavan1200@gmail.com>
2021-06-13 00:09:09 +0530

    implement API and user model

--- App.js
+++ App.js
@@ -1,17 +1,63 @@
-import React from 'react'
-import { View, Text } from 'react-native';
-import Amplify from 'aws-amplify'
-import config from './src/aws-exports.js';
-import { withAuthenticator } from 'aws-amplify-react-native'
+import React, { useEffect } from "react";
+import { View, Text } from "react-native";
+import Amplify, { Auth, API, graphqlOperation } from "aws-amplify";
+import config from "./src/aws-exports.js";
+import { withAuthenticator } from "aws-amplify-react-native";
+import { getUser } from "./src/graphql/queries";
+import { createUser } from "./src/graphql/mutations";
 
-Amplify.configure(config)
+Amplify.configure(config);
+
+const randomImages = [
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg",
+];
 
 const App = () => {
+  const getRandomImage = () => {
+    return randomImages[Math.floor(Math.random() * randomImages.length)];
+  };
+
+  // run this snippet only when App is first mounted
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      if (userInfo) {
+        const userData = await API.graphql(
+          graphqlOperation(getUser, { id: userInfo.attributes.sub })
+        );
+
+        if (userData.data.getUser) {
+          console.log("User is already registered in database");
+          return;
+        }
+
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri: getRandomImage(),
+          status: "Hey, I am using WhatsApp",
+        };
+
+        console.log(newUser);
+
+        await API.graphql(graphqlOperation(createUser, { input: newUser }));
+      }
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <View>
       <Text>Hello</Text>
     </View>
-  )
-}
+  );
+};
 
-export default withAuthenticator(App)
+export default withAuthenticator(App);
